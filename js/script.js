dc918c4806d51df4459275d173f653e564d6c2a9
gambitok <gambitokgd@gmail.com>
2018-03-20 15:10:40 +0200

    added fixed search, edited tab menu, changed font and color styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
     $(document).ready(function() {
         $('.menu-link').bigSlide();
 		
+		var $win = $(window),
+			$fixed = $(".fixed"),
+			limit = 120;
+
+		function tgl (state) {
+			$fixed.toggleClass("hidden", state);
+		}
+
+		$win.on("scroll", function () {
+			var top = $win.scrollTop();
+
+			if (top < limit) {
+				tgl(true);
+			} else {
+				tgl(false);
+			}
+		});
+		
     });
+
